43158f0788bc84881bdf84e8407d6b6522ccd618
Paulo Jr. Niche <pjniche@gmail.com>
2016-11-15 15:16:22 -0200

    Trabalho 3 iniciado

---
+++ 2016.2/fac/Trabalho 3/FAC - Trabalho 3.c
@@ -0,0 +1,26 @@
+//UFF - Universidade Federal Fluminense
+
+//Fundamentos de Arquitetura de Computadores
+//Prof.(a): Fernanda Passos
+
+//Trabalho 3
+//Paulo Roberto Niche Ramos Junior
+
+#include<stdio.h>
+
+void direto(){
+    //code
+}
+
+void associativo(){
+    //code
+}
+
+void associativo_conjuntos(){
+    //code
+}
+
+int main(void){
+    //programa
+    return 0;
+}
